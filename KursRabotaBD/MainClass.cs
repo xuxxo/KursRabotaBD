3ba73f3b33f3f3e56c0ec47ce352bb2c6c5fb575
alex_galaktica@mail.ru <alex_galaktica@mail.ru>
2022-05-03 19:04:16 +0300

    добавлено удаление и изменение данных

--- KursRabotaBD/MainClass.cs
+++ KursRabotaBD/MainClass.cs
@@ -2,30 +2,50 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Windows.Controls;
 
 namespace KursRabotaBD
 {
     internal class MainClass
     {
-        public static List<User> Lol()
+        UserContext db = new UserContext();
+        DataGrid _dtGrid;
+        public void FillTheGrid()
         {
-            using (UserContext db = new UserContext())
-            {
-                // создаем два объекта User
-                User user1 = new User { Name = "Tom", Age = 33 };
-                User user2 = new User { Name = "Sam", Age = 26 };
+            
+            
+            //    // создаем два объекта User
+            //    User user1 = new User { Name = "Tom", Age = 33 };
+            //    User user2 = new User { Name = "Sam", Age = 26 };
 
-                // добавляем их в бд
-                //db.Users.Add(user1);
-               // db.Users.Add(user2);
-                //db.SaveChanges();
-                Console.WriteLine("Объекты успешно сохранены");
-                db.Users.Load();
+            //    // добавляем их в бд
+            //    db.Users.Add(user1);
+            //    db.Users.Add(user2);
+            //    db.SaveChanges();
+                  db.Users.Load();
+                  
+                  _dtGrid.ItemsSource = db.Users.Local.ToBindingList();
+                  
                 // получаем объекты из бд и выводим на консоль
-                var users = db.Users.ToList();
-                return users;
-            }
+                
+        }
+         public void UpdateUsers()
+        {
+            db.SaveChanges();
+        }
 
+        public void DeleteUser(int id)
+        {
+            db.Users.Remove(db.Users.Find(id));
+        }
+        public void SaveChanges()
+        {
+            db.SaveChanges();
+        }
+
+        public void SetDataGrid(ref DataGrid dtGrid)
+        {
+            _dtGrid = dtGrid;
         }
     }
 }
--- KursRabotaBD/MainWindow.xaml.cs
+++ KursRabotaBD/MainWindow.xaml.cs
@@ -10,14 +10,38 @@ namespace KursRabotaBD
     /// </summary>
     public partial class MainWindow : Window
     {
+        MainClass mainClass;
         public MainWindow()
         {
             InitializeComponent();
-            dtGrid.ItemsSource = MainClass.Lol();
-           
+            mainClass = new MainClass();
+            mainClass.SetDataGrid(ref dtGrid);
+            mainClass.FillTheGrid();
+            
 
 
         }
-        
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainClass.UpdateUsers();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (dtGrid.SelectedItems.Count > 0)
+            {
+                for (int i = 0; i < dtGrid.SelectedItems.Count; i++)
+                {
+                    User user = dtGrid.SelectedItems[i] as User;
+                    if (user != null)
+                    {
+                        mainClass.DeleteUser(user.Id);
+                    }
+                }
+                mainClass.SaveChanges();
+            }
+            
+        }
     }
 }
